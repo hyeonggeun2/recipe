4870dca5e898842e47615daa2eb46018c80cdad9
hyeonggeun2 <dhgudrms12@gmail.com>
2021-06-04 22:20:57 +0900

    make model version1

--- app/members/models.py
+++ app/members/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+from recipes.models import Refrigerator
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=30)
+    refrigerator = models.OneToOneField(Refrigerator, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.refrigerator = Refrigerator.objects.create()
+        super().save(*args, **kwargs)
